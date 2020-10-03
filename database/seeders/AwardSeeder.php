670cbdf16903a06bc8a0bc74b3180a4ca4d684cb
slan48 <sergioagamezn@gmail.com>
2020-10-03 10:11:49 -0500

    Update images path in awards seeder

--- database/seeders/AwardSeeder.php
+++ database/seeders/AwardSeeder.php
@@ -16,7 +16,7 @@ class AwardSeeder extends Seeder
     {
         DB::table('awards')->insert([
             'name' => 'Renault Kwid',
-            'image_path' => 'awards/renault-bienvenida.jpg',
+            'image_path' => 'img/awards/renault-bienvenida.jpg',
             'stock' => 2,
             'available_date_start' => '2020-12-01',
             'available_date_end' => '2020-12-31'
@@ -24,7 +24,7 @@ class AwardSeeder extends Seeder
 
         DB::table('awards')->insert([
             'name' => 'Bóxer Cargo',
-            'image_path' => 'awards/boxer-cargo.jpg',
+            'image_path' => 'img/awards/boxer-cargo.jpg',
             'stock' => 5,
             'available_date_start' => '2020-10-01',
             'available_date_end' => '2020-12-31'
@@ -32,7 +32,7 @@ class AwardSeeder extends Seeder
 
         DB::table('awards')->insert([
             'name' => 'Cuartos fríos',
-            'image_path' => 'awards/cuarto-frio.jpg',
+            'image_path' => 'img/awards/cuarto-frio.jpg',
             'stock' => 3,
             'available_date_start' => '2020-10-01',
             'available_date_end' => '2020-12-31'
@@ -40,7 +40,7 @@ class AwardSeeder extends Seeder
 
         DB::table('awards')->insert([
             'name' => 'Kit de eventos',
-            'image_path' => 'awards/kit-eventos.jpg',
+            'image_path' => 'img/awards/kit-eventos.jpg',
             'stock' => 4,
             'available_date_start' => '2020-10-01',
             'available_date_end' => '2020-12-31'
@@ -48,7 +48,7 @@ class AwardSeeder extends Seeder
 
         DB::table('awards')->insert([
             'name' => 'TV LG 50"',
-            'image_path' => 'awards/tv-lg-50.jpg',
+            'image_path' => 'img/awards/tv-lg-50.jpg',
             'stock' => 9,
             'available_date_start' => '2020-10-01',
             'available_date_end' => '2020-12-31'
@@ -56,7 +56,7 @@ class AwardSeeder extends Seeder
 
         DB::table('awards')->insert([
             'name' => 'Bicicletas de domicilio',
-            'image_path' => 'awards/bici-domicilio.jpg',
+            'image_path' => 'img/awards/bici-domicilio.jpg',
             'stock' => 15,
             'available_date_start' => '2020-10-01',
             'available_date_end' => '2020-12-31'
